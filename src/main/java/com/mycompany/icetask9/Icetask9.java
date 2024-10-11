62902a4f4e3fe4977dff53623b04b0e20ab5b5e1
RC_Student_lab <RC_Student_lab@labG9AEB3.phdvvtwzx4oudioi2mg3lhtmwg.fx.internal.cloudapp.net>
2024-10-11 14:12:16 +0000

    1st commit

---
+++ src/main/java/com/mycompany/icetask9/Icetask9.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.icetask9;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Icetask9 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
